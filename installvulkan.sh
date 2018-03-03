89f8cb5ef2c9aaebc460546870859a23defb18c2
Thomas Crider <gloriouseggroll@gmail.com>
2018-03-03 01:54:46 -0500

    fix registry entries

--- installvulkan.sh
+++ installvulkan.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cp winevulkan.json $WINEPREFIX/drive_c/windows/
+
+WINEARCH=$WINEARCH WINEPREFIX=$WINEPREFIX wine regedit /S vulkan.reg
+
 if [ ! -f VulkanSDK-1.0.51.0-Installer.exe ]; then
-    wget https://vulkan.lunarg.com/sdk/download/1.0.51.0/windows/VulkanSDK-1.0.51.0-Installer.exe
+    curl https://vulkan.lunarg.com/sdk/download/1.0.51.0/windows/VulkanSDK-1.0.51.0-Installer.exe -o VulkanSDK-1.0.51.0-Installer.exe
 fi
 WINEARCH=$WINEARCH WINEPREFIX=$WINEPREFIX wine VulkanSDK-1.0.51.0-Installer.exe
-
-WINEARCH=$WINEARCH WINEPREFIX=$WINEPREFIX regedit vulkan.reg
 
 if [ -z "$DXVK" ]; then
     wineserver -k
